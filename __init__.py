4b985597faf43bbb71643bc601726597014d134e
bAleksiejczuk <106964945+bAleksiejczuk@users.noreply.github.com>
2025-06-14 00:09:25 +0200

    dekorator

--- __init__.py
+++ __init__.py
@@ -1,4 +1,20 @@
 from gui import start_gui
+import random
 
+#DEKORATOR
+def lucky_number_decorator(func):
+    def wrapper():
+        func()
+        lucky = random.randint(1, 100)
+        print(f"Hej twoja szczęśliwa liczba na dziś to: {lucky}")
+    return wrapper
+
+# Funkcja z dekoratorem
+@lucky_number_decorator
+def greet_user():
+    print("Cześć użytkowniku")
+
+# Wywołanie
 if __name__ == "__main__":
+    greet_user()
     start_gui()
